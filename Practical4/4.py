534699bbb4ca4b662deb6eadd5bf987b3d5a90a9
Yulia123-sys <56995312+Yulia123-sys@users.noreply.github.com>
2021-03-31 17:06:46 +0300

    Add files via upload

---
+++ Practical4/4.py
@@ -0,0 +1,30 @@
+import requests
+
+APPID = "248fa4c9af5a3ade3a9dac310308cc3a" 
+URL_BASE = "http://api.openweathermap.org/data/2.5/"
+
+
+def current_weather(q: str = "", appid: str = APPID) -> dict:
+    """https://openweathermap.org/api"""
+    return requests.get(URL_BASE + "weather", params=locals()).json()
+
+
+def weather_forecast(q: str = "", appid: str = APPID) -> dict:
+    """https://openweathermap.org/forecast5"""
+    return requests.get(URL_BASE + "forecast", params=locals()).json()
+
+
+def weather_onecall(lat: float = 55.68, lon: float = 12.57, appid: str = APPID) -> dict:
+    """https://openweathermap.org/api/one-call-api"""
+    return requests.get(URL_BASE + "onecall", params=locals()).json()
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    while True:
+        location = input("Введіть місто:").strip()
+        if location:
+            pprint(current_weather(location))
+        else:
+            break
